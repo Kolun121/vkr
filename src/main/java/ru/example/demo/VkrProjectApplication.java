36112133090051baae53c868234965756eca92a5
Nikolay <nicola.mosseev@yandex.ru>
2021-05-19 01:24:59 +0300

    Стартовый проект

---
+++ src/main/java/ru/example/demo/VkrProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VkrProjectApplication.class, args);
+	}
+
+}
